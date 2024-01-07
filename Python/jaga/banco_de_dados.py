f85051779ba72cd42e9b4d00bdd48870fbb510fd
Yuri Winycius <winyciusyuri@gmail.com>
2024-01-07 21:58:50 -0200

    Feat: Changed the file banco_de_dados.py

--- Python/jaga/banco_de_dados.py
+++ Python/jaga/banco_de_dados.py
@@ -4,13 +4,14 @@ class Banco_de_dados:
 
     def adicionar_ao_dicionario(self, chave: str, valor: str) -> dict:
         self.__set_dicionario(chave, valor)
-        self.get_dicionario()
+        return self.get_dicionario()
 
     def get_dicionario(self) -> dict:
         return self.__dicionario
     
     def __set_dicionario(self, chave: str, valor: str) -> None:
         self.__dicionario[chave] = valor
+        return self.get_dicionario()
         
     def remover_do_dicionario(self, chave) -> list:
         del self.__dicionario[chave]
